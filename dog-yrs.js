7c91503f54085bbeed01a107a0eaec3e94088aed
Filza Ahmad <64220634+filza-a@users.noreply.github.com>
2022-04-23 00:36:11 +0500

    Create dog-yrs.js

---
+++ dog-yrs.js
@@ -0,0 +1,15 @@
+//variable that contains my age
+let myAge = 20
+//variable that contains the early age
+let earlyYears = 2
+earlyYears = earlyYears * 10.5
+/*Since we already accounted for the first two years, taking the myAge variable, and subtract 2 from it.*/
+let laterYears = myAge - 2
+/* Multiply the laterYears variable by 4 to calculate the number of dog years accounted for by your later years.*/
+laterYears *= 4
+
+myAgeInDogYears = earlyYears + laterYears
+
+let myName = 'Filza'.toLowerCase()
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
